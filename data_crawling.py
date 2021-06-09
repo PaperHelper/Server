3edc8f1b3e8bb1a1297aae77c12a105c3dfdde77
nlee0212 <nlee0212@u.sogang.ac.kr>
2021-06-09 16:02:45 +0900

    merge summarizing & crawling into flask server

---
+++ data_crawling.py
@@ -0,0 +1,130 @@
+from requests import get  # to make GET request
+from selenium import webdriver
+import requests
+import time
+from bs4 import BeautifulSoup
+import re
+import os
+import json
+import tqdm
+
+pubs = {
+            'cs.ai': ['nips','ieee','eccv','cvpr','iccv','aaai','icml','iclr','iswc','neurlps','elsevier'],
+            'cs.db': ['sigir','sigmod','WWW','acm','ieee','icde','wsdm','cikm','icwsm'],
+            'cs.os': ['atc','usenix','acm','middleware','sosp','systor','osdi','eurosys','hotos','apsys','sac','ieee','springer'],
+            'cs.dc': ['acm','disc','dsn','ieee','debs','icdcs','icpads','ipdps','podc','ppopp','sirocco','spaa','srds','hipc','sc','elsevier'],
+            'cs.ni': ['globecom','ieee','icc','infocom','sigcomm','sigmetrics','acm','elsevier'],
+            'cs.cv': ['bmvc','cvpr','eccv','iccv','scia','ssiai','ieee','acmmm','iros','elsevier','springer'],
+            'cs.pl': ['ecoop','esop','acm','sigplan','icfp','iclp','oopsla','popl','pldi','elsevier'],
+            'cs.ds': ['esa','soda','acm','siam','swat','wads','waoa','ccc','fct','focs','ieee','icalp','isaac','mfcs','stacs','stoc','wollic'],
+            'cs.cl': ['acl','emnlp','naacl','tacl','SemEval','coling','eacl','conll','lrec','sigdial'],
+            'cs': ['acm','ieee','iclr','nips','icml','acs']
+        }
+
+def download(url, file_name):
+    with open(file_name, "wb") as file:   # open in binary mode
+        response = get(url)               # get request
+        file.write(response.content)      # write to file
+
+def get_random_papers():
+    return get_top_10_papers(['cs'])
+
+def get_top_10_papers(fields):
+    
+    def get_papers(fields,size):
+        URL = f"https://arxiv.org/search/advanced"
+        param = {
+                'advanced' : '',
+                'classification-computer_science' : 'y',
+                'date-date_type' : 'submitted_date',
+                'abstracts' : 'show',
+                'size' : '50',
+                'order' : '-announced_date_first'
+                }
+
+        cnt = 0
+        for field in fields:
+            for pub in pubs[field]:
+                param[f'terms-{cnt}-operator'] = 'AND' if cnt==0 else 'OR'
+                param[f'terms-{cnt}-term'] = field
+                param[f'terms-{cnt}-field'] = 'cross_list_category'
+                cnt += 1
+                param[f'terms-{cnt}-operator'] = 'AND'
+                param[f'terms-{cnt}-term'] = pub
+                param[f'terms-{cnt}-field'] = 'comments'
+                cnt += 1
+
+        response = requests.get(URL,params=param)
+        print(response)
+        print(response.url)
+        print(response.status_code)
+    
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        names = soup.find_all(attrs={'class':'list-title is-inline-block'})
+        codes = [re.findall('.*>arXiv:(.*)</a>\s',str(name))[0] for name in names]
+        titles = [t.text.strip() for t in soup.find_all(attrs={'class':'title is-5 mathjax'})]
+        authors = [[author.text.strip() for author in a.find_all(name='a')] for a in soup.find_all(attrs={'class':'authors'})]
+        comments = soup.find_all(attrs={'class':'has-text-grey-dark mathjax'})
+        tags = [t.text.strip().split('\n') for t in soup.find_all(attrs={'class':'tags is-inline-block'})]
+
+        mapper = dict()
+
+        for i in range(size):
+            mapper[codes[i]] = {
+                    'title':titles[i],
+                    'authors':authors[i],
+                    'publication':comments[i].text.strip(),
+                    'year':'20'+codes[i][:2],
+                    'tags':tags[i],
+                    'pdf':'https://arxiv.org/pdf/'+codes[i]+'.pdf'
+                    }
+
+        return mapper
+    
+    size = 50
+    mapper = dict()
+    new_mapper = get_papers(fields,size)
+
+    if os.path.exists('./mapper.json'):
+        with open('./mapper.json','r') as f:
+            mapper = json.load(f)
+
+    mapper.update(new_mapper)
+
+    with open('./mapper.json','w') as f:
+       json.dump(mapper,f)
+
+    while True:
+        ret = check_served(mapper)
+        if ret != -1:
+            break
+        mapper.update(get_titles_codes(fields,size+10))
+        size += 10
+
+    print(ret)
+    return ret 
+
+def check_served(mapper):
+    if not os.path.exists('./served_papers_list.txt'):
+        return list(mapper.keys())[:10]
+
+    with open('./served_papers_list.txt','r') as f:
+        served_codes = f.read().split('\n')
+
+    new_codes = [c for c in mapper.keys() if c not in served_codes]
+    if len(new_codes) >= 10:
+        return new_codes[:10]
+    else:
+        return -1
+
+def get_files(papers):
+    if not os.path.exists('./data/'):
+        os.mkdir('./data/')
+    for p in papers:
+        download(f'https://arxiv.org/pdf/{p}.pdf',f'./data/{p}.pdf')
+        print(f'{p}.pdf download completed.')
+
+if __name__ == '__main__':
+    papers = get_top_10_papers(['cs.cl'])
+    get_files(papers)
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
 import json
+from paper_summarization import main
+from data_crawling import get_top_10_papers,get_files,get_random_papers
+import os
+from threading import Thread
+
 #from flask_restx import Api, Resource
 
 app = Flask(__name__)
@@ -11,8 +16,13 @@ POST data (json)
     'interest': 'cs.ai\tcs.cr'
 }
 '''
+
+interest = ''
+
 @app.route('/interest', methods=['POST'])
 def postInterest():
+    global interest
+
     cs = {"Artificial Intelligence":"cs.ai", 
             "Database":"cs.db", 
             "Operating Systems":"cs.os", 
@@ -22,7 +32,7 @@ def postInterest():
             "Robotics":"cs.ro",
             "Programming Languages":"cs.pl",
             "Data Structures and Algorithms":"cs.ds",
-            "Cryptography and Security":"cs.cr"
+            "Computation and Language":"cs.cl"
             }
 
     data = request.get_json(silent=True, cache=False, force=True)
@@ -31,7 +41,7 @@ def postInterest():
     interest = data['interest']
     for key in cs.keys():
         interest = interest.replace(key, cs[key])
-    
+   
     print("Interests:", interest)
     return jsonify(interest)
 
@@ -39,52 +49,95 @@ def postInterest():
 '''
 GET data (json)
 {
-    'paper': [
-        {
-            "title": "title1",
-            "author": "author1",
-            "publication": "publication1",
-            "year": "year1",
-            "summary": "summary1",
-            "pdf": "pdf1"
-        },
-        {
-            "title": "title2",
-            "author": "author2",
-            "publication": "publication2",
-            "year": "year2",
-            "summary": "summary2",
-            "pdf": "pdf2"
-        },
-        {   
-            "title": "title3",
-            "author": "author3",
-            "publication": "publication3",
-            "year": "year3",
-            "summary": "summary3",
-            "pdf": "pdf3"
-        }
-    }
+    "title0": "title0",
+    "author0": "author0",
+    "publication0": "publication0",
+    "year0": "year0",
+    "summary0": "summary0",
+    "pdf0": "pdf0"
+    "title1": "title1",
+    "author1": "author1",
+    "publication1": "publication1",
+    "year1": "year1",
+    "summary1": "summary1",
+    "pdf1": "pdf1"
+    "title2": "title2",
+    "author2": "author2",
+    "publication2": "publication2",
+    "year2": "year2",
+    "summary2": "summary2",
+    "pdf2": "pdf2"
+}
 '''
+
+def send_summary(fields):
+    count = 0
+
+    with open('./summary_data_ready.json') as f:
+        summary_data = json.load(f)
+
+    paper = {}
+
+    with open('./served_papers_list.txt','a') as f:
+        for k in list(summary_data.keys()):
+            if count == 3:
+                break
+            if fields[0] != '':
+                if len(set(summary_data[k][tags]) & set(fields)) == 0:
+                    continue
+            f.write(k+'\n')
+            data = summary_data[k]
+            ccount = str(count)
+            paper['title' + ccount] = data['title']
+            paper['author' + ccount] = ','.join(data['author'])
+            paper['publication' + ccount] = data['publication']
+            paper['year' + ccount] = data['year']
+            paper['summary' + ccount] = data['summary']
+            paper['pdf' + ccount] = data['pdf']   # pdf URL!
+            count += 1
+
+    return paper
+
+def summarization_caching(fields):
+    if len(interest) == 0:
+        paper_list = get_random_papers()
+    else:
+        paper_list = get_top_10_papers(fields)
+
+    # download papers for summarization
+    get_files(paper_list)
+
+    # summarize given papers
+    main(paper_list)
+
 @app.route('/paper', methods=['GET'])
 def getPaper():
-    json_object = {'paper': []}
+    paper = {}
 
-    for i in range(3):
-        paper = {}
-        paper['title'] = 'title' + str(i)
-        paper['author'] = 'author' + str(i)
-        paper['publication'] = 'publication' + str(i)
-        paper['year'] = 'year' + str(i)
-        paper['summary'] = 'summary' + str(i)
-        paper['pdf'] = 'pdf' + str(i)   # pdf URL!
-        json_object['paper'].append(paper)
+    fields = interest.strip().split('\t')
+    print(fields)
 
+    if os.path.exists('./summary_data_ready.json'):
+        paper = send_summary(fields)
+        paper = json.dumps(paper)
+        print(paper)
 
-    json_object = json.dumps(json_object)
-    print(json_object)
+    else:
+        print('here!')
+        for i in range(3):
+            paper['title'+str(i)] = 'please wait'
+            paper['author'+str(i)] = ''
+            paper['publication'+str(i)] = ''
+            paper['year'+str(i)] = ''
+            paper['summary'+str(i)] = ''
+            paper['pdf'+str(i)] = ''
 
-    return jsonify(json_object)
+    thread = Thread(target=summarization_caching,args=(fields,))
+    thread.daemon = True
+    thread.start()
+    paper = json.dumps(paper)
+    print(paper)
+    return jsonify(paper)
 
 if __name__ == "__main__":
     app.run(debug=True, host="163.239.28.25", port=5000)
